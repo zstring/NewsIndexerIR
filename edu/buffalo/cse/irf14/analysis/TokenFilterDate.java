19e873b871d3f013de426f5310066d0f63340a4e
zstring <jai.himanshu001@gmail.com>
2014-09-15 00:38:46 -0400

    Added basic functionality for extracting and setting date value

--- edu/buffalo/cse/irf14/analysis/TokenFilterDate.java
+++ edu/buffalo/cse/irf14/analysis/TokenFilterDate.java
@@ -3,6 +3,9 @@
  */
 package edu.buffalo.cse.irf14.analysis;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author inspire
  *
@@ -25,7 +28,64 @@ public class TokenFilterDate extends TokenFilter {
 		// TODO Auto-generated method stub
 		if (stream.hasNext()) {
 			Token token = stream.getCurrent();
+			String tkString = token.toString();
+			int tkNum = -1;
+			try {
+				tkNum = Integer.parseInt(tkString);
+			} catch (NumberFormatException e) {
+			}
+			if (tkNum >= 1000 && tkNum <= 3000) {
+				
+				Token[] prevTokens = new Token[2];//stream.getPrevFiveToken();
+				String m = prevTokens[3].toString();
+				String d = prevTokens[4].toString();
+				String[] fetchedMonthDate = fetchMonthDate(m, d);
+				if (!fetchedMonthDate[0].equals(Boolean.TRUE.toString())) {
+					fetchedMonthDate = fetchMonthDate(d, m);
+				}
+				token.setTermText(tkString + fetchedMonthDate[1]);
+			}
 		}
-		return false;
+		return stream.hasNext();
+	}
+
+	private String[] fetchMonthDate(String m, String d) {
+		String[] retVal = new String[2];
+		Boolean flag = false;
+		String monthRegex = "(jan)|(feb)|(mar)|(apr)|(may)|(jun)|"
+				+ "(jul)|(aug)|(sep)|(oct)|(nov)|(dec)";
+		String digitRegex = "\\d+";
+		String dateIndex = "01";
+		String monthIndex = "01";
+		Matcher mat = Pattern.compile(monthRegex,
+				Pattern.CASE_INSENSITIVE).matcher(m);
+		if (mat.find()) {
+			for (int i = 1; i <= 12; i++) {
+				if (mat.group(i) != null) {
+					monthIndex = i + "";
+					flag = true;
+					if (monthIndex.length() == 1) {
+						monthIndex = "0" + monthIndex;
+					}
+					break;
+				}
+			}
+			mat = Pattern.compile(digitRegex).matcher(d);
+			if (mat.matches()) {
+				dateIndex = d;
+				flag = true;
+				if (dateIndex.length() == 1) {
+					dateIndex = "0" + dateIndex;
+				}
+			}
+		}
+		if (flag) {
+			retVal[0] = Boolean.TRUE.toString();
+		}
+		else {
+			retVal[0] = Boolean.FALSE.toString();
+		}
+		retVal[1] = monthIndex + dateIndex;
+		return retVal;
 	}
 }
