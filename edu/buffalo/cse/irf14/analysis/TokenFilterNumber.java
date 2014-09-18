e6e1bcbe3a1829e226c397f7aca47f22b1884843
avinav <sharan.avinav@gmail.com>
2014-09-18 16:08:25 -0400

    updated numbers

--- edu/buffalo/cse/irf14/analysis/TokenFilterNumber.java
+++ edu/buffalo/cse/irf14/analysis/TokenFilterNumber.java
@@ -15,17 +15,32 @@ public class TokenFilterNumber extends TokenFilter{
 	@Override
 	public boolean increment() throws TokenizerException {
 		// TODO Auto-generated method stub
-		/* 1. Does token contains AM/PM/BC./AD/month name
-		 * 2. Does next token contains AM/P.M./B.C./AD
+		/* 0. If a token contains a number literal. Proceed else quit.
+		 * 1. Does token contains AM/PM/BC./AD or month name
+		 * 2. Does next token contains AM/P.M./B.C./AD or month name
 		 * 3. Does previous token contains month name
 		 * 4. if 1,2,3 fails. Is number 4 digit and between 1000 and 3000
 		 * 5. if 1,2,3,4 fails: remove number digits only. if empty string remains, remove token. 
 		 */
 		if (stream.hasNext()) {
 			Token token = stream.next();
+			boolean isDate = false;
+			String datePattern = "(\\W)([AapP].?[mM].?(\\W)|(\\W)([bB].?[cC].?)(\\W)|(\\W)([Aa].?[dD].?(\\W))";
+			String datePatternNextToken = "(^|\\s)+([AapP].?[mM].?)(\\W)|(^|\\s)([bB].?[cC].?)(\\W)|(\\s|^)([Aa].?[dD].?(\\W))";
+			String monthPattern = "((jan)|(feb)|(mar)|(apr)|(may)|(jun)|"
+					+ "(jul)|(aug)|(sep)|(oct)|(nov)|(dec))";
 			String tkString = token.toString();
-			if (tkString.matches("")) {
-				
+			if (tkString.matches("[0-9]")) {
+				if (tkString.matches(datePattern)) {
+					isDate = true;
+				}
+				else if (stream.next().toString().matches(datePatternNextToken+"|"+monthPattern)) {
+					isDate = true;
+					stream.previous();
+				}
+				else if (stream.previous().toString().matches("")) {
+					
+				}
 			}
 		}
 			
