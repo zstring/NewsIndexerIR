a936db98796a769acd9062b44029b4a90dfdffa2
avinav <sharan.avinav@gmail.com>
2014-09-27 04:23:41 -0400

    tokenFilterNumber: implemented Date

--- edu/buffalo/cse/irf14/analysis/TokenFilterNumber.java
+++ edu/buffalo/cse/irf14/analysis/TokenFilterNumber.java
@@ -3,6 +3,7 @@
  */
 package edu.buffalo.cse.irf14.analysis;
 
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,6 +12,19 @@ import java.util.regex.Pattern;
  *
  */
 public class TokenFilterNumber extends TokenFilter{
+	private static Pattern pattIsNum, pattDate, pattYear, pattMonth, pattNumRem,pattTimeYear;
+	static {
+		pattIsNum = Pattern.compile("[^a-zA-z]*[0-9][^a-zA-Z]*");
+		pattDate = Pattern.compile("[0-9 ]?[AapP]\\.?[mM]\\.?(\\W|$)|"
+				+ "[0-9 ]?[bB]\\.?[cC]\\.?(\\W|$)|[0-9 ]?[Aa]\\.?[dD]\\.?(\\W|$)");
+//		pattYear = Pattern.compile("\\W[12]\\d{3}\\W");
+		pattNumRem = Pattern.compile("(,|\\.)?[0-9]");
+		pattMonth = Pattern.compile("january|february|march|april|may|june|"
+				+ "july|august|september|october|november|december|jan|feb|mar|apr"
+				+ "|jun|jul|aug|sep|nov|dec");
+//		pattTimeYear = Pattern.compile("((^| )[AapP]\\.?[mM].?\\W)|((^| )[bB]\\.?[cC].?\\W)|"
+//				+ "(( |^)[Aa]\\.?[dD]\\.?\\W)");
+	}
 	public TokenFilterNumber(TokenStream stream) {
 		super(stream);
 	}
@@ -35,10 +49,10 @@ public class TokenFilterNumber extends TokenFilter{
 			}
 			if (token != null) {
 				boolean isDate = false;
-				String datePattern = "(\\W[AapP].?[mM].?\\W)|(\\W[bB].?[cC].?\\W)|(\\W[Aa].?[dD].?\\W)";
-				String datePatternNextToken = "((^|\\s)[AapP].?[mM].?\\W)|((^|\\s)[bB].?[cC].?\\W)|((\\s|^)[Aa].?[dD].?\\W)";
-				String monthPattern = "((january)|(february)|(march)|(april)|(may)|(june)|"
-						+ "(july)|(august)|(september)|(october)|(november)|(december))";
+				//String datePattern = "(\\W[AapP].?[mM].?\\W)|(\\W[bB].?[cC].?\\W)|(\\W[Aa].?[dD].?\\W)";
+//				String datePatternNextToken = "((^|\\s)[AapP].?[mM].?\\W)|((^|\\s)[bB].?[cC].?\\W)|((\\s|^)[Aa].?[dD].?\\W))";
+//				String monthPattern = "((january)|(february)|(march)|(april)|(may)|(june)|"
+//						+ "(july)|(august)|(september)|(october)|(november)|(december))";
 				String tkString = token.toString();
 				int currIndex = stream.getCurrentIndex();
 				/*Matcher matcher = Pattern.compile("[(A-Za-z)(\\d)(,\\.)(\\W)]+",Pattern.CASE_INSENSITIVE).matcher(tkString);
@@ -54,8 +68,20 @@ public class TokenFilterNumber extends TokenFilter{
 			if (tkString.matches("^[a-zA-z]*[0-9]^[a-zA-Z]*")) {
 
 			}*/
-				if (tkString.matches("[^a-zA-z]*[0-9][^a-zA-Z]*")) {
-					if (tkString.matches(datePattern)) {
+				//if (tkString.matches("[^a-zA-z]*[0-9][^a-zA-Z]*")) {
+				if (pattIsNum.matcher(tkString).matches()) {
+					//if (tkString.matches(datePattern)) {
+					Token[] prevTokens = stream.getPrevTokens(2);
+					StringBuilder prevTokenString = new StringBuilder("");
+					for (int i = 0; i < prevTokens.length; i++) {
+						if (prevTokens[i] != null) {
+							prevTokenString.append(prevTokens[i].toString().toLowerCase()+" ");
+						}
+					}
+					if (pattDate.matcher(prevTokenString).find()) {
+						isDate = true;
+					}
+					else if (pattMonth.matcher(prevTokenString).find()) {
 						isDate = true;
 					}
 //					else if (stream.hasNext() && stream.getTokenAt(currIndex + 1).toString().matches(datePatternNextToken+"|"+monthPattern)) {
@@ -64,11 +90,13 @@ public class TokenFilterNumber extends TokenFilter{
 //					else if (stream.hasPrevious() && stream.getTokenAt(currIndex-1).toString().matches(monthPattern)) {
 //						isDate = true;
 //					}
-					else if(tkString.matches("\\W[12]\\d{3}\\W")) {
-						isDate = true;
-					}
+					//else if(tkString.matches("\\W[12]\\d{3}\\W")) {
+//					else if (pattYear.matcher(tkString).matches()) {
+//						isDate = true;
+//					}
 					else {
-						tkString = tkString.replaceAll("(,|\\.)?[0-9]", "");
+//						tkString = tkString.replaceAll("(,|\\.)?[0-9]", "");
+						tkString = pattNumRem.matcher(tkString).replaceAll("");
 					}
 				}
 				if ("".equals(tkString.trim())) {
