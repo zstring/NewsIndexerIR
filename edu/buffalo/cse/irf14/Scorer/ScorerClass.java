7d77e4baa544574dc7e9b35e0dcfd3130e15fefd
zstring <jai.himanshu001@gmail.com>
2014-10-14 22:57:13 -0400

    Updated the scorer class for Similarity vector based model using tf-idf

--- edu/buffalo/cse/irf14/Scorer/ScorerClass.java
+++ edu/buffalo/cse/irf14/Scorer/ScorerClass.java
@@ -3,15 +3,17 @@ package edu.buffalo.cse.irf14.Scorer;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 import edu.buffalo.cse.irf14.SearchRunner.ScoringModel;
 import edu.buffalo.cse.irf14.index.Posting;
 
 public class ScorerClass {
 
-	public void rankResult (Map<String, Posting> unRankedResult,
+	public HashMap<String, Double> rankResult (Map<String, Posting> unRankedResult,
 			HashMap<String, HashMap<Integer, Double>> docVector,
 			Map<Integer, Double> queryVector, ScoringModel model) {
+		HashMap<String, Double> rankedResult = new HashMap<String, Double>();
 		Set<Integer> termKeys = queryVector.keySet();
 		for (String docId : unRankedResult.keySet()) {
 			Double sum = 0.0;
@@ -19,6 +21,9 @@ public class ScorerClass {
 			for (Integer termId : termKeys) {
 				sum += docV.get(termId) * queryVector.get(termId);
 			}
+			rankedResult.put(docId, sum);
 		}
+		return rankedResult;
 	}
+
 }
