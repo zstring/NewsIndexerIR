3646858c6af2a3038b26cda245929fe5b3a6e497
zstring <jai.himanshu001@gmail.com>
2014-10-18 00:09:30 -0400

    1. BaseIndexer : Updated the Document Vector, Now contains substring of the token also but with less Weight eg. "san francisco" will have three tokens 1. "san francisco" 2. "san" and 3. "francisco" 2. ScorerClass : Added comments for Comparator Class

--- edu/buffalo/cse/irf14/Scorer/ScorerClass.java
+++ edu/buffalo/cse/irf14/Scorer/ScorerClass.java
@@ -36,6 +36,10 @@ public class ScorerClass {
 		sortedRankedRes.putAll(rankedResult);
 		return sortedRankedRes;
 	}
+	/*
+	 * Comaparator for TreeMap
+	 * Sorting the keys based on values
+	 */
 	public class SortByValueComp implements Comparator<String> {
 		Map<String, Double> map;
 		public SortByValueComp(Map<String, Double> map) {
@@ -43,6 +47,10 @@ public class ScorerClass {
 		}
 		@Override
 		public int compare(String a, String b) {
+			//Intentionally comparaing two String objects
+			//to check weather they are same object or 
+			//not if same then return 0 else compare the value 
+			//of another string to first one
 			if (a == b) 
 				return 0;
 			Double aValue = map.get(a);
